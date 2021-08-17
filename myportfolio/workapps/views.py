ac16adcaed4757cb41a166a90d5278a926608276
swallahfx <adeniyisaheed75@gmail.com>
2021-08-17 21:34:57 +0100

    finally

--- myportfolio/workapps/views.py
+++ myportfolio/workapps/views.py
@@ -4,10 +4,26 @@ from .models import Home, About, Profile, Category, Skills, Portfolio
 
 
 def index(request):
-
+    #home
     home = Home.objects.latest('updated')
+
+    #About
+    about = About.objects.latest('updated')
+    profiles = Profile.objects.filter(about=about)
+
+    #skills
+    categories = Category.objects.all()
+
+    #portfolio
+    portfolios = Portfolio.objects.all()
+
+
     context = {
         'home': home,
+        'about': about,
+        'profiles': profiles,
+        'categories': categories,
+        'portfolios': portfolios,
     }
 
     return render(request, 'index.html', context)
